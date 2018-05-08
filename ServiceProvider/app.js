c7b329d76868d610c91db15d7fba688afa2538ff
kiiayla <kyla.kate@gmail.com>
2018-05-08 21:26:07 +0800

    rendering pages -- backend

--- ServiceProvider/app.js
+++ ServiceProvider/app.js
@@ -5,7 +5,7 @@ Object.values = Object.values || (obj => Object.keys(obj).map(key => obj[key]));
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
-var sql = require('mysql');
+var mysql = require('mysql');
 
 
 var app = express();
@@ -28,8 +28,9 @@ app.listen(1234, function(){
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
+	port: 3306,
 	password: '',
-	database: 'cargo_database'
+	database: 'cargo database'
 });
 
 connection.connect(function(err){
@@ -38,18 +39,25 @@ connection.connect(function(err){
 });
 
 
-
 app.get('/', function (req, res){
-	res.sendFile(__dirname + "/" + "index.html"); // to handle home page requests 
+	res.sendFile(__dirname + "/index.html"); // to handle home page requests 
 });
 
-app.get('/main', function(req, res){
-
-});
 
 app.get('/login', function(req, res){
 	res.render('login');
 });
+
+app.post('/login', function(req, res){
+	var username = req.body.username;
+
+	connection.query("SELECT * FROM user JOIN customer on user.userID = customer.custID")
+})
+
+app.get('/main', function(req, res){
+	res.render('main');
+});
+
 
 
 app.get('/logout', function(req, res){
@@ -64,53 +72,12 @@ app.get('/error', function(req, res){
 	res.render('error');
 });
 
-app.post('/login', function(req, res){
-	var username = req.body.username;
-	var password = req.body.password;
-
-	connection.query("SELECT *FROM users JOIN customer on user.userID = customer.custID WHERE username = 'username' AND password = 'password'", function(req, rows){
-		if (err) { console.error(err); return }
-
-		var result = false;
-		console.log('rows');
-		rows.forEach(function (item) {
-			result = true;
-
-
-		})
-	}) 
-
-})
-
-
-var userName = null;
-app.get('/password', function(req, res){
-	userName = req.query.username;
-	res.render('password');
+app.get("/create-services", function(req, res){
+	res.render('create-services');
 });
 
-app.post('/password', function(req, res){
-	var password = req.body.password; 
-	var username = username;
-
-	connection.query("SELECT username, password FROM users WHERE status = 1", function(err, rows){
-		if (err) { console.error(err); return }
-
-		var valid = false;
-		rows.forEach(function(item){
-			if(item.username == username && item.password == password){
-				var userType = item.userType;
-				valid = true;
-
-				req.session.username = username;
-				res.redirect('/main');
-			}
-		});
-
-		if(!valid){
-			res.redirect('/error');
-		}
-	});
+app.get("/show-reservation", function(req, res){
+	res.render('show-reservation');
 });
 
 
